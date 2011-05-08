8652ed0433a7666c52264df185c2132a1e1664c2
Abel Martin <abel.martin@gmail.com>
2011-05-08 14:22:43 -0400

    Added a date dropdown for the movies table that  starts with the current month and ticks all the way backwards to the January 1 of the previous year

--- cotm.rb
+++ cotm.rb
@@ -2,7 +2,7 @@ require 'rubygems' if RUBY_VERSION < '1.9'
 require 'sinatra/base'
 require 'haml'
 require 'net/http'
-require 'uri'
+require 'date'
 
 # You want your API key in a proxy call of some sort so it's not publicly visible.
 # I'm placing my actaul API key in a seperate file OUTSIDE of source control.
@@ -11,6 +11,7 @@ require './my_api_key'
 class CotM < Sinatra::Base
 
   get '/' do
+    @current_month = Date.today.month
     haml :index
   end
 
@@ -19,7 +20,10 @@ class CotM < Sinatra::Base
     api_url = "http://api.nytimes.com/svc/movies/v2/reviews/search.json?"
 
     # Your params
-    api_url += "&opening-date=2011-05-06"
+    api_url += "&opening-date=#{params[:start_date]};#{params[:end_date]}"
+
+    # Default Sort by openning day
+    api_url += "&order=by-opening-date"
 
     # The API from 'my_api_key.rb'
     api_url += "&api-key=#{API_KEY}"
